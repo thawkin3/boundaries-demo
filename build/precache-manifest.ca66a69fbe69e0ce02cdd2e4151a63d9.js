da2c90ba3cab3e3cea12e7d4c6d43e3cc40bf17f
Tyler Hawkins <tylerhawkins@workfront.com>
2021-11-05 15:06:15 -0600

    chore: builds v1 of the app

---
+++ build/precache-manifest.ca66a69fbe69e0ce02cdd2e4151a63d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d2d7dfb30d5c927474663faeea6f7b8",
+    "url": "/boundaries-demo/build/index.html"
+  },
+  {
+    "revision": "0be9e5e061e6ad64a889",
+    "url": "/boundaries-demo/build/static/css/main.c42468e7.chunk.css"
+  },
+  {
+    "revision": "e7becbe9f18780106c10",
+    "url": "/boundaries-demo/build/static/js/2.8a89ce7c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/boundaries-demo/build/static/js/2.8a89ce7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0be9e5e061e6ad64a889",
+    "url": "/boundaries-demo/build/static/js/main.df3d40f6.chunk.js"
+  },
+  {
+    "revision": "54af088968fd7f258021",
+    "url": "/boundaries-demo/build/static/js/runtime-main.4f8b0336.js"
+  }
+]);
